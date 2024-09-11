427f1d0a1b95da7f9c321e4e9fa1388864b992e5
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-09-11 11:00:42 +0000

    I did the task and the extension

--- task.py
+++ task.py
@@ -3,8 +3,9 @@ def task():
     result = ["Matthew", "Mark", "Luke"]
 
     # Code here
-
-    # --------- #
-
+    x = input('Please enter a name: ')
+    x = x.capitalize()
+    result.append(x)
+        # --------- #
 
     return result
